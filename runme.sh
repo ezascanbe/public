376515b20b134cede89498571d5920f811cfa079
ezascanbe <jmantell@gmail.com>
2023-01-20 16:01:26 +1100

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -7,18 +7,18 @@ LICENSE=cloudian_289001406012.lic
 WORKINGPATH=/home/cloudian
 STAGING=/opt/cloudian-staging
 
-function do_stuff () {
-        /usr/bin/chmod +x $WORKINGPATH/$FILE
+function run_install () {
+        chmod +x $WORKINGPATH/$FILE
         ./$FILE $LICENSE
-        /usr/bin/cp survey.csv $STAGING/$VERSION/
+        cp survey.csv $STAGING/$VERSION/
         $STAGING/$VERSION/cloudianInstall.sh -b -s survey.csv configure-dnsmasq force-warnings
 }
 
 if [ -f "$FILE" ]; then
     echo "$FILE binary for $VERSION already exists."
-    do_stuff
+    run_install
 else
     echo "$FILE does not exist. Downloading $VERSION..."
-    /usr/bin/curl -L https://s3.cloudianhyperstore.com/downloads/HyperStore/7/$VERSION/$FILE --output $WORKINGPATH$FILE
-    do_stuff
+    curl -L https://s3.cloudianhyperstore.com/downloads/HyperStore/7/$VERSION/$FILE --output $WORKINGPATH/$FILE
+    run_install
 fi
