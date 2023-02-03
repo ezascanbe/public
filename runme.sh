03ddf37a2c5c6a0cfef9ac87659e4a670ae1625d
ezascanbe <jmantell@gmail.com>
2023-02-03 17:51:03 +1100

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -16,14 +16,20 @@ function run_install () {
         $STAGING/$VERSION/cloudianInstall.sh -b -s survey.csv configure-dnsmasq force-warnings topleveldomain=$PUBDNSSUFFIX
 }
 
+function get_patches () {
+    curl -L https://bit.ly/40kRJu5 --output $WORKINGPATH/S3Patch-7.4.2.1.bin
+    chmod +x $WORKINGPATH/S3Patch-7.4.2.1.bin
+    curl -L https://bit.ly/3jpSXU0 --output $WORKINGPATH/HSConfigPatch-7.4.2.1.bin
+    chmod +x $WORKINGPATH/HSConfigPatch-7.4.2.1.bin
+}
+
 if [ -f "$FILE" ]; then
     echo "$FILE binary for $VERSION already exists."
+    get_patches
     run_install
 else
     echo "$FILE does not exist. Downloading $VERSION..."
     curl -L https://s3.cloudianhyperstore.com/downloads/HyperStore/7/$VERSION/$FILE --output $WORKINGPATH/$FILE
-    curl -L https://bit.ly/40kRJu5 --output $WORKINGPATH/S3Patch-7.4.2.1.bin
-    curl -L https://bit.ly/3jpSXU0 --output $WORKINGPATH/HSConfigPatch-7.4.2.1.bin
     #UK mirror uncomment to use
     #curl -L  https://s3-uk-1.downloads.cloudian.eu/data/HyperStore/7/$VERSION/$FILE --output $WORKINGPATH/$FILE
     run_install
